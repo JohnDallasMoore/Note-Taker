e6b14e93b311e0e6916d30ee838e9a154252358b
Dallas Moore <moore.johndallas@gmail.com>
2023-06-02 13:27:48 -0500

    added delete function and corrected route directery

--- routers/notes.js
+++ routers/notes.js
@@ -1,6 +1,6 @@
 const notesRouter = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readFromFile,  readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
 
 //Route to add saved notes
 notesRouter.get('/', (req, res) => {
@@ -29,6 +29,14 @@ notesRouter.post('/', (req, res) => {
 notesRouter.delete("/:id", (req, res) => {
     const noteId = req.params.id;
     //figure out wtf to put here
-});
-
+    //Delete the note by ID?
+    readFromFile('./db/db.json')
+        .then((data) => {
+            const notes = JSON.parse(data);
+            const updatedNotes = notes.filter((note) => note.id !== noteId);
+            //write updated notes to DB
+            writeToFile('./db/db.json', updatedNotes);
+            res.json(`Note deleted successfully 🚀`);
+        });
+    });
 module.exports = notesRouter;
